a5611ebe95844a2afa141d638ce4ac6e94fe69d4
seorin <befly098@gmail.com>
2025-08-10 17:36:05 +0900

    [feat] 미사용 파일 + 코드 제거

--- src/main/java/com/redis/testbed/seorin/controller/MemberController.java
+++ src/main/java/com/redis/testbed/seorin/controller/MemberController.java
@@ -1,19 +1,14 @@
 package com.redis.testbed.seorin.controller;
 
-import javax.naming.AuthenticationNotSupportedException;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.redis.testbed.seorin.entity.MemberEntity;
-import com.redis.testbed.seorin.entity.MemberLoginRequest;
 import com.redis.testbed.seorin.entity.MemberRegisterRequest;
 import com.redis.testbed.seorin.service.MemberService;
 
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @RestController
@@ -26,13 +21,6 @@ public class MemberController {
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
-
-	// @PostMapping("/login") // TODO: spring security 적용, jwt 토큰 발급
-	// public void login(@RequestBody @Valid MemberLoginRequest memberLoginRequest, HttpSession httpSession) throws
-	// 	ResponseStatusException {
-	// 	MemberEntity member = memberService.login(memberLoginRequest.toLogin());
-	// 	httpSession.setAttribute(SESSION_NAME, member.getUsername());
-	// }
 
 	@PostMapping("/register")
 	public String register(@RequestBody @Valid MemberRegisterRequest memberRegisterRequest) {
